abc07f06eeafca71e891cb8f4a9e7a50486b9055
brianOlaya <brilot777@gmail.com>
2020-05-05 19:56:33 -0600

    Commit incrementar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+
 import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  
+  state ={
+    count :0,
+  }
+
+  incrementar = () =>{
+  this.setState({ count: this.state.count + 1})
+  }
+
   render() {
     return (
-      <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+      <div className ="App">
+        <p>{this.state.count}</p>
+        <button onClick={this.incrementar}>Incrementa</button>
       </div>
     );
   }
